27acfb04586627a8696069d204716f9bd20482ea
sett <dr.cat911@generative.xyz>
2024-01-22 16:57:07 +0700

    add visual

--- src/modules/home2d/index.tsx
+++ src/modules/home2d/index.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import s from './styles.module.scss'
 import Button from '@/components/Button'
+import {CDN_URL_VIDEOS} from "@/modules/constant";
 
 export default function Home2d() {
   return (
     <div className={s.homepage}>
       <div className={s.homepage_bg}>
-        <Image src='/img/bg-home.jpg' width={1920} height={1080} alt='bg-home' />
+        <video width={1920} height={1080} autoPlay playsInline loop muted>
+          <source src={`${CDN_URL_VIDEOS}/original-33532573c947eaea79d5ffbd86bfddbe.mp4`}/>
+        </video>
       </div>
       <div className={s.homepage_inner}>
         <div className={s.homepage_content}>
