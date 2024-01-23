3f29d10cd4f1007e46d6974b1d51083d55bd7eb2
Jackie <1121@generative.xyz>
2024-01-23 18:00:20 +0700

    update: add outline effect

--- src/modules/workshop/index.tsx
+++ src/modules/workshop/index.tsx
@@ -1,18 +1,24 @@
+import { useStoreGlobal } from '@/stores'
 import Three3D from './Three3D'
 import UI from './UI'
 import PreviewRoom from './components/Preview'
 import s from './styles.module.scss'
 
 export default function WorkShop() {
+  const { viewPreview } = useStoreGlobal()
+
   return (
     <main className={s.workshop}>
       <div className={s.workshop_main}>
         <UI />
         <Three3D />
       </div>
-      <div className={s.workshop_preview}>
-        <PreviewRoom />
-      </div>
+      {viewPreview && (
+        <div className={s.workshop_preview}>
+          {' '}
+          <PreviewRoom />
+        </div>
+      )}
     </main>
   )
 }
