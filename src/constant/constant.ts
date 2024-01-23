45abd8065aaf15e527f5958a4332f4b5c0b0d6a0
sett <dr.cat911@generative.xyz>
2024-01-23 17:58:55 +0700

    fix build

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -121,3 +121,18 @@ export const BLAST_CONTRACT = process.env.NEXT_PUBLIC_BLAST_CONTRACT!;
 export const AA_TRACKING_API_KEY = process.env.NEXT_PUBLIC_AA_TRACKING_API_KEY!;
 
 export const AGORA_APP_ID = process.env.NEXT_PUBLIC_AGORA_APP_ID!;
+
+const MIN_DECIMAL = 2;
+const MAX_DECIMAL = 6;
+const NATIVE_ETH_ADDRESS = '0x0000000000000000000000000000000000000000';
+const METAMASK_DOWNLOAD_PAGE = 'https://metamask.io/download/';
+
+export { MIN_DECIMAL, MAX_DECIMAL, NATIVE_ETH_ADDRESS, METAMASK_DOWNLOAD_PAGE };
+
+export const ALLOWED_ATTRIBUTES = {
+    '*': ['style'],
+    span: ['class'],
+    a: ['href', 'target'],
+    img: ['src', 'width', 'height'],
+    iframe: ['src', 'width', 'height'],
+};
--- src/utils/px2rem.ts
+++
@@ -1,3 +0,0 @@
-const px2rem = (size: number | string) => `${Number(size) / 10}rem`;
-
-export default px2rem;
