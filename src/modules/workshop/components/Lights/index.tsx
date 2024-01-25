1f05d2e0a25b83ab7bc372d4266f5cd2a2b120db
Jackie <1121@generative.xyz>
2024-01-25 18:12:39 +0700

    update: light intensity

--- src/modules/workshop/components/Lights/index.tsx
+++ src/modules/workshop/components/Lights/index.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export const Lights = () => {
   return (
     <>
-      <hemisphereLight intensity={2} />
-      <hemisphereLight intensity={10} color={0xdf901a} />
-      <directionalLight position={[0, 30, 0]} intensity={5}></directionalLight>
-      <directionalLight position={[30, -5, 0]} intensity={5}></directionalLight>
-      <directionalLight position={[0, 0, 30]} intensity={5}></directionalLight>
+      <hemisphereLight intensity={15} />
+      <hemisphereLight intensity={15} color={0xdf901a} />
+      <directionalLight position={[0, 30, 0]} intensity={1.2}></directionalLight>
+      <directionalLight position={[30, -5, 0]} intensity={2}></directionalLight>
+      <directionalLight position={[0, 0, 30]} intensity={1.2}></directionalLight>
       <directionalLight position={[-30, 0, 0]} intensity={1.2}></directionalLight>
     </>
   )
