7093dade0c432a66e9402e6cc78da53e7aa3651e
Max <max@gmail.com>
2024-01-25 09:55:34 +0700

    update

--- src/modules/workshop-2d/index.tsx
+++ src/modules/workshop-2d/index.tsx
@@ -3,6 +3,7 @@
 
 import { useEffect, useMemo, useRef, useState } from 'react'
 import style from './styles.module.scss'
+import Img from 'next/image';
 
 export default function Workshop2D() {
   const [valueRange, setValueRange] = useState<number>(0)
@@ -66,19 +67,66 @@ export default function Workshop2D() {
 
   return (
     <div className={style.workshop2d}>
-      <div className={`${style.container} grid grid-cols-12`}>
-        <div className={`${style.box} col-span-6`}>
-          <input ref={inputRef} type='file' accept='image/*' onChange={uploadInput} />
-          <p>Original Image:</p>
-          <img src='' alt='uploaded image' ref={uploadImageRef} />
-          <p>Pixelated Image: </p>
-          <canvas width={300} height={300} ref={pixelatedImageRef} />
+      <div className={style.container}>
+        <div className={`${style.container_heading} flex items-center`}>
+          <h2>Use image</h2>
+          <p>Project you have build</p>
+
+
         </div>
-        <div className={`${style.input} col-span-6`}>
-          <p>Pixelation: </p>
-          <input type='range' min={1} max={100} value={valueRange} ref={rangePixelRef} onInput={pixelRangeHandler} />
+
+        <div className={`grid grid-cols-12 mt-5`}>
+          <div className="col-span-5">
+            <div className="flex flex-col items-start">
+              <div className="">
+                <label className={`${style.picture}`} htmlFor="picture__input" tabIndex="0">
+                  <span className={`${style.picture__image}`}>Upload image</span>
+                </label>
+                <input type="file" name="picture__input" id={`${style.picture__input}`} />
+              </div>
+              <div className="mt-14">
+                <label className={`${style.picture}`} htmlFor="picture__input" tabIndex="0">
+                  <span className={`${style.picture__image}`}>Upload image</span>
+                </label>
+                <input type="file" name="picture__input" id={`${style.picture__input}`} />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-7">
+            <div className="flex flex-col items-start">
+              <div className="">
+                <label className={`${style.picture}`} htmlFor="picture__input" tabIndex="0">
+                  <span className={`${style.picture__image}`}>Upload image</span>
+                </label>
+                <input type="file" name="picture__input" id={`${style.picture__input}`} />
+              </div>
+              <div className="mt-14">
+                <label className={`${style.picture}`} htmlFor="picture__input" tabIndex="0">
+                  <span className={`${style.picture__image}`}>Upload image</span>
+                </label>
+                <input type="file" name="picture__input" id={`${style.picture__input}`} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={`grid grid-cols-12`}>
+          <div className={`${style.box} col-span-6`}>
+            <p>Original Image:</p>
+            <Img src="" width={256} height={253} alt="uploaded image" ref={uploadImageRef} />
+            <p>Pixelated Image: </p>
+            <canvas width={300} height={300} ref={pixelatedImageRef} />
+          </div>
+          <div className={`${style.input} col-span-6`}>
+            <input ref={inputRef} type='file' accept='image/*' onChange={uploadInput} />
+            <p>Pixelation: </p>
+            <input type='range' min={1} max={100} value={valueRange} ref={rangePixelRef} onInput={pixelRangeHandler} />
+          </div>
         </div>
       </div>
+
+
     </div>
   )
 }
