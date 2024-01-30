8a38eeb5f1ce14274b46493a63e40c3bd201870a
richter <richter@newbitcoincity.com>
2024-01-30 13:42:58 +0700

    fix: logo top header don't load

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import s from './styles.module.scss'
-import Image from 'next/image'
+import Link from 'next/link'
+
 import Fade from '@/interactive/Fade'
 import WalletButton from '@/components/WalletButton'
 import { usePathname } from 'next/navigation'
 import cn from 'classnames'
+import LogoIcon from '/public/imgs/logo.svg'
 
 export default function Header() {
   const pathname = usePathname()
@@ -14,7 +16,9 @@ export default function Header() {
     <header className={s.header}>
       <div className={cn(isWorkshop ? s.containerWorkshop : s.container)}>
         <div className={`${s.logo} js-header-logo`}>
-          <Image width={40} height={40} src={'imgs/logo.svg'} alt={'logo'} />
+          <Link href='/'>
+            <LogoIcon />
+          </Link>
           <Fade delay={1}>Modular</Fade>
         </div>
         <WalletButton />
