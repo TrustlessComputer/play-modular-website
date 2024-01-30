78827360f2fffdad6c79c13603f4e49520b02e85
Max <max@gmail.com>
2024-01-30 17:56:16 +0700

    update svg

--- src/modules/workshop/components/Brick/index.tsx
+++ src/modules/workshop/components/Brick/index.tsx
@@ -39,7 +39,6 @@ export const Brick = ({
     z: dimensions.z % 2 === 0 ? dimensions.z / 2 : (dimensions.z - 1) / 2,
   }
   const isSelected2 = selectedBricks.find((brick) => brick.userData.uID === uID) ? true : false
-
   const offset = {
     x: Math.sign(translation.x) < 0 ? Math.max(translation.x, -compansate.x) : Math.min(translation.x, compansate.x),
     z: Math.sign(translation.z) < 0 ? Math.max(translation.z, -compansate.z) : Math.min(translation.z, compansate.z),
@@ -179,7 +178,7 @@ export const Brick = ({
                   scale={[
                     brickGeometry.length > 1 ? base * 2.5 : base * 2.5,
                     (base * 2) / 1.5,
-                    brickGeometry.length > 1 ? base * 2 : base,
+                    brickGeometry.length > 1 ? base * 2 : base * 2,
                   ]}
                 >
                   <meshPhysicalMaterial
