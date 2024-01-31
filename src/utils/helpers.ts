153bf985cd4fd8b6b5cead2d7f8e7be2ada68788
Archaon <archaon1574@trustless.computer>
2024-01-31 11:08:49 +0700

    feat: add export image btn

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -121,3 +121,17 @@ export function isBlank(str) {
 export const isBrowser = (): boolean => {
   return typeof window !== 'undefined'
 }
+
+// Using fetch
+export async function downloadImage(imageSrc, name) {
+  const image = await fetch(imageSrc)
+  const imageBlog = await image.blob()
+  const imageURL = URL.createObjectURL(imageBlog)
+
+  const link = document.createElement('a')
+  link.href = imageURL
+  link.download = name
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
