2b8d1fa5e89d83b8ccd3c8d3b253d6540615c7d2
luin <1103@generative.xyz>
2024-01-31 11:28:35 +0700

    update link bvm

--- src/modules/home2d/Banner/index.tsx
+++ src/modules/home2d/Banner/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import s from './style.module.scss'
 import { IconBVM } from '@/components/IconSvgs'
+import Link from 'next/link'
 
 const Banner = () => {
   return (
-    <div className={s.banner}>
+    <Link href={'https://bvm.network/'} className={s.banner}>
       <div className={s.banner_content}>
         <IconBVM />
         <p>Powered by blockchain Bitcoin Virtual Machine</p>
       </div>
-    </div>
+    </Link>
   )
 }
 
