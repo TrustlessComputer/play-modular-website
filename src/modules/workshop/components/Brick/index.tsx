f9aa4da11d88a7c64ab74e628cc142be72b1ea8f
Max <max@gmail.com>
2024-02-01 12:26:32 +0700

    update

--- src/modules/workshop/components/Brick/index.tsx
+++ src/modules/workshop/components/Brick/index.tsx
@@ -204,7 +204,7 @@ export const Brick = ({
               />
               {!isNontTexture && (
                 <Decal
-                  position={[0, 0, dimensions.x == 2 ? base + 0.005 : 13 + 0.005  ]}
+                  position={[0, 0, dimensions.x == 2 ? base + 0.005 : 13 + 0.005]}
                   rotation={[0, 0, 0]}
                   scale={[
                     base * 3,
@@ -224,7 +224,8 @@ export const Brick = ({
                   {/*  polygonOffset*/}
                   {/*  polygonOffsetFactor={-1}*/}
                   {/*/>*/}
-                  <meshStandardMaterial map={texturez} polygonOffset roughness={1} metalness={0.35} color={'#fff'} emissive={'#000'}  alphaHash={true} transparent alphaTest={0}/>
+                  <meshStandardMaterial map={texturez} polygonOffset roughness={1} metalness={0.35} color={'#fff'}
+                                        emissive={'#000'} alphaHash={true} transparent alphaTest={0} />
                 </Decal>
               )}
             </mesh>
