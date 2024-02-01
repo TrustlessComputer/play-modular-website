7fcc242071b45c441ae6496093879596490db3ce
sett <dr.cat911@generative.xyz>
2024-02-01 13:24:28 +0700

    ui banner

--- src/modules/home2d/Banner/index.tsx
+++ src/modules/home2d/Banner/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import s from './style.module.scss'
 import { IconBVM } from '@/components/IconSvgs'
 import Link from 'next/link'
-
+import Countdown from "@/components/Countdown";
+import dayjs from "dayjs";
+import {HOME_URL, WORKSHOP_URL} from "@/constant/route-path";
+import {usePathname} from "next/navigation";
 const Banner = () => {
+    const pathname = usePathname()
+    const isHome = useMemo((): boolean=>{
+        return pathname === '/'
+    }, [pathname])
   return (
-    <Link href={'https://bvm.network/public-sale?source=playmodular'}  target='_blank' rel='noopener noreferrer' className={s.banner}>
+    <Link href={'https://bvm.network/public-sale?source=playmodular'}  target='_blank' rel='noopener noreferrer' className={`${s.banner} ${isHome && s.isHome}`}>
       <div className={s.banner_content}>
         <IconBVM />
-        <p>Powered by Bitcoin Virtual Machine</p>
+        <div>Exclusive for Modular Builders: <strong>20% Boost</strong> for $BVM Public Sale within the next <Countdown
+            className={s.countdown}
+            expiredTime={dayjs
+                .utc('Fri Feb 02 2024 03:30:00 GMT+0000')
+                .toString()}
+        />!</div>
+          {pathname === HOME_URL && <span className={s.workshopBtn}> Join Now </span>}
       </div>
     </Link>
   )
